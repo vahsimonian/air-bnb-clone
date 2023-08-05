0ed1370653d793f6d8f8a810537f52f3c7588cf0
vahsimonian <simonianvah@gmail.com>
2023-08-05 12:57:18 +0700

    commit

--- src/PlaceImg.jsx
+++ src/PlaceImg.jsx
@@ -1,23 +1,11 @@
-import React from 'react'
+import Image from './Image'
 
 export default function PlaceImg({ place, index = 0, className = null }) {
   if (!place.photos?.length) {
     return ''
   }
-
   if (!className) {
     className = 'object-cover'
   }
-
-  return (
-    <div>
-      {place.photos.length > 0 && (
-        <img
-          className={className}
-          src={'http://localhost:4000/uploads/' + place.photos[index]}
-          alt=''
-        />
-      )}
-    </div>
-  )
+  return <Image className={className} src={place.photos[index]} alt='' />
 }
